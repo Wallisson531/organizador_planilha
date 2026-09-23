d3df771fc45a1a51ce0c257c536ba9daf67b9a11
Wallisson Daniel <wallissondaniel19@gmail.com>
2026-09-23 11:36:40 -0300

    Projeto de automação para tarefas diarias

--- organizador_planilha.py
+++ organizador_planilha.py
@@ -1,9 +1,10 @@
 import os
-from datetime import datetime
 import tkinter as tk
+from datetime import datetime
 from tkinter import filedialog, messagebox, ttk
 
 import pandas as pd
+
 
 class LimpadorPlanilha:
     def __init__(self, janela):
@@ -61,4 +62,88 @@ class LimpadorPlanilha:
         )
         self.log.pack(pady=10)
 
-        
+    def escrever_log(self,menssagem):
+        self.log.insert(tk.END, menssagem + "\n")
+        self.log.see(tk.END)
+
+    def selecionar_arquivo(self):
+        self.arquivo = filedialog.askopenfilename(
+            title='Selecione a Planilha',
+            filetypes=[('Excel', '*.xlsx *.xls')]
+        )
+        if self.arquivo:
+            self.label_arquivo.config(text=self.arquivo)
+            self.escrever_log("Arquivo selecionado com sucesso.")
+
+    def processar(self):
+        if not self.arquivo:
+            messagebox.showwarning(
+                'Atenção',
+                'Selecione uma planilha primeiro!'
+            )
+            return
+        try:
+            self.progresso['value'] = 20
+
+            self.escrever_log('Lendo planilha...')
+            df = pd.read_excel(self.arquivo)
+            self.progresso['value'] = 40
+
+            colunas_desejadas = [
+                'FZ',
+                'MODELO',
+                'FM',
+                'CLIENTE',
+                'Carro',
+                'DATA DE CÁSULO',
+                'QTD'
+            ]
+
+            self.escrever_log('Filtrando colunas...')
+            df_filtrando = df[colunas_desejadas]
+
+            self.progresso['value'] = 70
+
+            downloads = os.path.join(
+                os.path.expanduser('~'),
+                'Downloads'
+            )
+
+            nome_arquivo = (
+                f'Planilha_limpa_'
+                f"{datetime.now().strftime('%d_%m_%Y_%H_%M_%S')}.xlsx"
+            )
+
+            arquivo_saida = os.path.join(
+                downloads,
+                nome_arquivo
+            )
+
+            self.escrever_log('Salvando arquivo...')
+
+            df_filtrando.to_excel(
+                arquivo_saida,
+                index=False
+            )
+
+            self.progresso['value'] = 100
+
+            self.escrever_log(
+                f'Arquivo salvo com sucesso: {arquivo_saida}'
+            )
+
+            messagebox.showinfo(
+                'Sucesso',
+                f'Planilha gerada com sucesso!\n\n{arquivo_saida}'
+            )
+
+        except Exception as erro:
+            messagebox.showerror(
+                'Erro',
+                str(erro)
+            )
+
+janela = tk.Tk()
+app =LimpadorPlanilha(janela)
+
+janela.mainloop()
